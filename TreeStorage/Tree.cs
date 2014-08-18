20d4d7d2810d370b73965c3ac77b06415b609f43
demmy <demmy.uber@gmail.com>
2014-08-18 15:36:06 +0300

    связанный список

--- TreeStorage/Tree.cs
+++ TreeStorage/Tree.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Abstract;
 
@@ -6,21 +7,44 @@ namespace TreeStorage
 {
     public class Tree<T> : BaseTree<T>, ITree<T>
     {
+        private Tree<T> head;
+        private Tree<T> next;
+        private T data;
+
+        private Tree(T data, Tree<T> head, Tree<T> next)
+        {
+            this.data = data;
+            this.head = head;
+            this.next = next;
+        }
+        public Tree():this(default(T),null,null)
+        {
+        }
+
         public override IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var current = this;
+            while (current != null)
+            {
+                yield return current.data;
+                current = current.next;
+            }
         }
 
         public override object Clone()
         {
-            throw new NotImplementedException();
+            return new Tree<T>(data, head, next);
         }
 
-        public override T Data { get; set; }
+        public override T Data
+        {
+            get { return this.data; }
+            set { this.data = value; }
+        }
 
         public override IBaseTree<T> Parent
         {
-            get { throw new NotImplementedException(); }
+            get { return this.head; }
         }
 
         public override IList<IBaseTree<T>> Find(Comparison<T> c)
@@ -30,17 +54,38 @@ namespace TreeStorage
 
         public override IList<IBaseTree<T>> GetDirectChildren()
         {
-            throw new NotImplementedException();
+            var current = this.next;
+            IList<IBaseTree<T>> list = new List<IBaseTree<T>>();
+            while (current != null)
+            {
+                list.Add(current);
+                current = current.next;
+            }
+            return list;
         }
 
         public ITree<T> AddChild(T data)
         {
-            throw new NotImplementedException();
+            var child = new Tree<T>();
+            child.head = this;
+            child.data = data;
+            this.next = child;
+            return child;
         }
 
         public bool RemoveChild(ITree<T> t)
         {
-            throw new NotImplementedException();
+            var current = this.next;
+            while (current != null)
+            {
+                if (current.Equals(t))
+                {
+                    current.head.next = current.next;
+                    return true;
+                }
+                current = current.next;
+            }
+            return false;
         }
     }
 }
--- TreeTask/Program.cs
+++ TreeTask/Program.cs
@@ -1,9 +1,36 @@
-﻿namespace TreeTask
+﻿using System;
+using System.Collections.Generic;
+using Abstract;
+using TreeStorage;
+
+namespace TreeTask
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
+          ITree<Person> p1 = new Tree<Person>();
+
+            p1.Data = new Person(){Name = "John"};
+            var p2 = p1.AddChild(new Person() {Name = "Mary"});
+            var p3 = p2.AddChild(new Person() {Name = "Jane"});
+
+            foreach (var person in p1)
+            {
+                Console.WriteLine(person.Name);
+            }
+            Console.WriteLine("--------------");
+            foreach (var person in p2)
+            {
+                Console.WriteLine(person.Name);
+            }
+
+
+
+
+
         }
     }
+
+
 }
